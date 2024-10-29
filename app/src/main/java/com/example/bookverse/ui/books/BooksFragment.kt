b202625976e9c318da00e41fc4e3f3a8bbe7528a
nirmala <nirmalasilalahi8@gmail.com>
2024-10-30 04:45:58 +0700

    Perubahan Pertama

--- app/src/main/java/com/example/bookverse/ui/books/BooksFragment.kt
+++ app/src/main/java/com/example/bookverse/ui/books/BooksFragment.kt
@@ -1,4 +1,4 @@
-package com.example.bookverse.ui.dashboard
+package com.example.bookverse.ui.books
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.bookverse.databinding.FragmentDashboardBinding
+import com.example.bookverse.databinding.FragmentBooksBinding
 
-class DashboardFragment : Fragment() {
+class BooksFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentBooksBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val booksViewModel =
+            ViewModelProvider(this).get(BooksViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentBooksBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textNotifications
+        booksViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
